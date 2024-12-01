faae63c37a9a1807d6a893781febebcc413d749f
snowypy <git@snowyjs.lol>
2024-12-01 15:04:39 +0000

    Add Flashcard and Course entities to AppDataSource

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,14 @@
 import { DataSource } from 'typeorm';
 import { User } from './entities/User';
 import { InviteCode } from './entities/InviteCode';
+import { Flashcard } from './entities/Flashcard';
+import { Course } from './entities/Course';
 
 export const AppDataSource = new DataSource({
     type: 'mongodb',
     url: process.env.MONGO_URL,
     database: 'superstudy',
     synchronize: true,
-    entities: [User, InviteCode],
+    entities: [User, InviteCode, Flashcard, Course],
     useUnifiedTopology: true,
 });
